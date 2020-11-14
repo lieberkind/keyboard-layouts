229bb0520b7fa3b8a3e1685c9c8dbbf14510b2bd
Tomas Lieberkind <tl@famly.co>
2020-11-14 17:49:37 +0100

    Add forward slash and question mark to symbol layer

--- levinson-rev3/keymap.c
+++ levinson-rev3/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_SYMBOL] = LAYOUT_ortho_4x12( \
   KC_GRAVE,KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC, KC_CIRC, KC_AMPR, KC_PAST, KC_LPRN, KC_RPRN, KC_EQUAL, \
-  _______, _______, _______, _______, ALFRED , _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_PLUS, \
+  _______, _______, _______, _______, ALFRED , _______, _______, KC_SLSH, KC_QUES, KC_LCBR, KC_RCBR, KC_PLUS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_PIPE, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TILD, _______, KC_BSLS \
 ),
